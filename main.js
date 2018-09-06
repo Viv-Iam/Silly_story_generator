c6de9a312a4defba98be75c01d47d409208b6e7a
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-06 23:44:38 +0300

    Replace the three placeholders in the newStory

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var customName = document.getElementById('customname');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-var insertY = ["the soup kitchen", "Disneyland", "the White House"];
-var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+var storyText = 'It was 94 fahrenheit outside, so ' + xItem + ' went for a walk. When they got to ' + yItem + ' , they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — ' + zItem + ' weighs 300 pounds, and it was a hot day.';
+var insertX = ['Willy the Goblin", "Big Daddy", "Father Christmas'];
+var insertY = ['the soup kitchen", "Disneyland", "the White House'];
+var insertZ = ['spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away'];
 var newStory = storyText;
 var xItem = randomValueFromArray(insertX);
 var yItem = randomValueFromArray(insertY);
