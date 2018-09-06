7a1c20ca5d30cae337596ae4b08725363c8fae63
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-06 21:02:10 +0300

    Add var x, y and zitem equate them to randomValueFromArray => arrays added earlier

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var customName = document.getElementById('customname');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 var insertY = ["the soup kitchen", "Disneyland", "the White House"];
 var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+var newStory = storyText;
+var xItem = randomValueFromArray(insertX);
+var yItem = randomValueFromArray(insertY);
+var zItem = randomValueFromArray(insertZ);
 
 function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
