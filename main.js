25e9786d6d04a794d57b4cd61d365b0ba70a4c40
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-07 19:41:42 +0300

    *Update script link in index **Add comments to js file

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Declare variables
 var customName = document.getElementById('customname');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
@@ -6,33 +7,48 @@ var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 var newStory = storyText;
+
+// Call randomValueFromArray function and pass specific arrays as arguments
 var xItem = randomValueFromArray(insertX);
 var yItem = randomValueFromArray(insertY);
 var zItem = randomValueFromArray(insertZ);
 
+// Replace text with text from array
 newStory.replace('insertx', xItem);
 newStory.replace('inserty', yItem);
 newStory.replace('insertz', zItem);
 
-function randomValueFromArray(array){
-  return array[Math.floor(Math.random()*array.length)];
+
+function randomValueFromArray(array) {
+
+  // Randomly get array index
+  return array[Math.floor(Math.random() * array.length)];
 
   randomize.addEventListener('click', result);
-  
+
   function result() {
-  
-    if(customName.value !== '') {
+
+    if (customName.value !== '') {
       var name = customName.value;
       newStory.replace('Bob', name);
     }
-  
-    if(document.getElementById("uk").checked) {
-      var weight = Math.round(300/14) + ' stone';
-      var temperature =  Math.round((94-32)*(5/9)) + ' centigrade';
-  
+
+    // Check if UK radio button is checked
+    if (document.getElementById("uk").checked) {
+
+      // Convert weight from pounds to stone
+      var weight = Math.round(300 / 14) + ' stone';
+
+      // Convert temperature from Fahrenheit to Celsius
+      var temperature = Math.round((94 - 32) * (5 / 9)) + ' centigrade';
+
+      // Replace temperature and weight with converted values
+      newStory.replace('94 fahrenheit', temperature);
+      newStory.replace('300 pounds', weight);
+
     }
-  
-    story.textContent = ;
+
+    story.textContent = newStory;
     story.style.visibility = 'visible';
   }
 }
